fbfa98ff7b9c343ce8d50dd1dc1dc54aad91aaf5
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-10-01 12:36:21 +0900

    OkHttp

--- app/src/main/java/com/example/socketpractice/MainActivity.kt
+++ app/src/main/java/com/example/socketpractice/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 
 import android.util.Log
 import com.example.socketpractice.databinding.ActivityMainBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.*
 
 import java.net.Socket
@@ -13,18 +18,37 @@ import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val client = OkHttpClient()
+
+        val request: Request = Request.Builder()
+            .url("http://10.0.2.2:8080")   //에뮬 테스트
+            .build()
 
 
+        val callback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                //요청에 실패했을때
+                Log.e("Client", e.toString())
+            }
 
+            override fun onResponse(call: Call, response: Response) {
+                //요청에 성공했을때
+                if (response.isSuccessful) {
+                    //요청에 성공하고 데이터도 성공적으로 받아올때
+                    Log.e("Client", "${response.body?.string()}")//body부분가져옴
+                }
+            }
 
+        }
 
+        client.newCall(request).enqueue(callback)// 큐로 쌓아놨다가 넣는순서대로 동작 처리 하고 onFail 이나 onResponse 로 데이터 넘겨주게됨
 
 
         /*Thread {
